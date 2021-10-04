622ab6a195d6e3c5814019b4fa52f9a63226f18d
2muchopo <opomaintain@gmail.com>
2021-10-04 14:43:27 -0700

    Adjusted some codes
    Adjusted some codes to specify the appropriate comments.

--- OlogboBMI.js
+++ OlogboBMI.js
@@ -20,13 +20,14 @@ button.addEventListener('click', function (e) {
             comment = 'You are underweight and may need to build some muscles and eat more food.';
             console.log('small')
           break;
-        case (18.5 < bmi < 25):
-           comment = 'Very good, you are fit, you may just need to maintain your routine exercises and diet. Bravo!!!';
+        case (bmi > 25 && bmi < 29.9) :
+            comment =  'Your bmi says you are overweight, you may need to take more intense gym routines, check your diet or better still, consult your doctor for better medical advice'
+             ;
+            break;
+          
+        case (bmi > 18.5 && bmi < 25):
+           comment = 'Very, you are fit, you may just need to maintain your routine exercises and diet. Bravo!!!';
         break;
-        case (25 < bmi < 29.9) :
-          comment =  'Your bmi says you are overweight, you may need to take more intense gym routines, check your diet or better still, consult your doctor for better medical advice'
-           ;
-          break;
         case (bmi > 29.9) :
          comment = 'Your bmi says you are Obese, you may need to take more intense gym routines, check your diet or better still, consult your doctor for better medical advice';
           break;
